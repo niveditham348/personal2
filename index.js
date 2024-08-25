889afa522356606b12e3f02d6f179520a0e610d8
niveditham348 <nm5062020@gmail.com>
2024-08-25 19:35:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+
+  if (randomNumber1===5){
+    document.querySelector("h1").innerHTML = "player 1 wins!";
+  }
+  else if(randomNumber2===6){
+    document.querySelector("h1").innerHTML = "player 2 Wins!";
+  }
+  else{
+    document.querySelector("h1").innerHTML = "no one Wins!"
+  }
+    
